514e2d2f80fe7a5b1d97f4860bd8cfeef3db5726
pphein <pph.hein87@gmail.com>
2023-06-16 07:42:35 +0630

    Changed socket

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+var express = require('express');
+var socket = require('socket.io');
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+// App setup
+var app = express();
+var server = app.listen(8000, function () {
+    console.log('listening for requests on port 8000, http://localhost:8000');
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
-});
+// Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+    console.log("Someone connected id", socket.id);
+
+    socket.on('saMal', function (data) {
+        io.sockets.emit('saMal', data);
+    });
+
+    socket.on('wayMal', function (data) {
+        io.sockets.emit('wayMal', data);
+    })
+
+})
